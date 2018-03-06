b171457f1f2b36f2ebd0228f0c161c4ab7b13ccd
Daniil Velichko <danvelduck@gmail.com>
2018-03-06 09:53:20 +0800

    Add routing with outlets

--- demo/main.ts
+++ demo/main.ts
@@ -1,20 +1,76 @@
-import {createApp, Model, ComponentTemplate} from '../src/aludel'
+import {createApp, Model, ComponentTemplate, ComponentConfig} from '../src/aludel'
 import m from 'mithril'
 
 const {createComponent, start} = createApp(
     (rootElement: HTMLElement, component: any) => m.render(rootElement, component()),
-    {message: 'Hello'}
+    {message: 'Hello', tasks: { list: [{name: 'First task'}] }}
 )
 
-const homeTemplate: ComponentTemplate = {
+const outerTemplate: ComponentTemplate = {
     sockets: ['text'],
     actions: {
-        '@init': () => (model) => model.set('text', 'Init'),
+        '@init': () => (model) => model.set('text', 'Lol'),
         add: () => (model) => model.set('text', model.text + '!')
     },
-    render: ({model, actions}) => m('div', {onclick: actions.add}, model.text),
+    render: ({model, actions, outlet}) =>
+        m('div', {onclick: actions.add}, [model.text, outlet()]),
 }
 
-const homeComponent = createComponent(homeTemplate, {'text': ['message']})
+const outerConfig: ComponentConfig = {
+    template: outerTemplate,
+    paths: {
+        'text': ['message']
+    }
+}
 
-start(document.body, homeComponent)
+const tasksTemplate: ComponentTemplate = {
+    sockets: ['list', 'info'],
+    actions: {
+        '@init': () => (model) => model.set('info', 'Tasks lol')
+    },
+    render: ({model, outlet}) => m('div', [model.info , outlet(), 'List end'])
+}
+
+const tasksConfig = {
+    template: tasksTemplate,
+    paths: {
+        'list': ['tasks', 'list'],
+        'info': ['tasks', 'info']
+    }
+}
+
+const taskTemplate: ComponentTemplate = {
+    sockets: ['list', 'taskId'],
+    actions: {},
+    render: ({model, outlet}) => m('div', ['Task start', model.taskId, 'Task end'])
+}
+
+const taskConfig = {
+    template: taskTemplate,
+    paths: {
+        'list': ['tasks', 'list'],
+        'taskId': ['tasks', 'selectedId']
+    }
+}
+
+const routes = {
+    '/': '/tasks',
+    '/tasks': {
+        name: 'Tasks', 
+        component: tasksConfig, 
+        subroutes: {
+            '/:id': {
+                name: 'Task',
+                component: taskConfig,
+                action: (params) => (model: Model) => {
+                    console.log(params)
+                    console.log(model)
+                    return model.set('taskId', params.id)
+                }
+            }
+        }
+
+    }
+}
+
+start(document.body, outerConfig, routes)
--- src/aludel.ts
+++ src/aludel.ts
@@ -38,10 +38,12 @@
 import flyd from 'flyd'
 import Immutable from 'seamless-immutable'
 import Mapper from 'url-mapper'
+import createHistory from 'history/createBrowserHistory'
 
 export type Model = Immutable.ImmutableObject<{[key: string]: any}>
 export type UpdateFn = (model: Model) => Model
 export type RendererFn = (rootElement: HTMLElement, component: Component) => any
+export type RouteRendererFn = (component: Component) => any
 
 export interface ComponentTemplate {
     sockets: string[]
@@ -63,20 +65,27 @@ export interface RenderTools {
 export type Component = () => any
 export type RenderFn = (tools: RenderTools) => any
 export type Action = (...args: any[]) => (model: Model) => Model
+export type RouteAction = (params: {[key: string]: any}) => (model: Model) => Model
 export type ActionMap = {[key: string]: Action}
 export type SocketMap = {[key: string]: string[]}
 
-export type RouteMap = {[key: string]: Route}
+export type RouteMap = {[key: string]: Route | string}
 export interface Route {
     name: string
     component: ComponentConfig
-    subroutes: RouteMap
+    action?: RouteAction
+    subroutes?: RouteMap
 }
 
 export type FlatRouteMap = {[key: string]: FlatRoute}
+export interface FlatRouteCache {
+    component: Component
+}
 export interface FlatRoute {
-    path: string
     name: string
+    cache?: Component
+    components: ComponentConfig[]
+    actions: RouteAction[]
 }
 
 export const createApp = (
@@ -84,11 +93,16 @@ export const createApp = (
     initialModel: {[key: string]: any},
 ) => {
     // Step over whole routes tree and create components injected with outlet functions that depend on relevant route changes
+    //
+    const urlMapper = Mapper()
+
     const updateStream = flyd.stream<UpdateFn>()
 
-    const applyUpdate = (currentModel: Model, modelUpdate: UpdateFn) => modelUpdate(currentModel)
+    const applyUpdate = 
+        (currentModel: Model, modelUpdate: UpdateFn) => modelUpdate(currentModel)
 
-    const modelStream = flyd.scan<Model, UpdateFn>(applyUpdate, Immutable(initialModel), updateStream)
+    const modelStream =
+        flyd.scan<Model, UpdateFn>(applyUpdate, Immutable(initialModel), updateStream)
 
     const localModel = (sockets: string[], paths: SocketMap) =>
         sockets.reduce(
@@ -105,15 +119,24 @@ export const createApp = (
             global
         )
 
-    const createComponent = (template: ComponentTemplate, paths: SocketMap): Component => {
+    const createComponent = 
+        (template: ComponentTemplate, paths: SocketMap, outlet: Component): Component => {
         const model = () => localModel(template.sockets, paths)
+
+        // TODO: Make sure all sockets have defined paths
 
         const actions = Object
             .keys(template.actions)
             .reduce(
                 (acc, action) => {
-                    const postActionModel = (...args: any[]) => template.actions[action](...args)(localModel(template.sockets, paths))
-                    acc[action] = (...args: any[]) => updateStream(syncModel(postActionModel(...args), template.sockets, paths))
+                    const postActionModel = (...args: any[]) =>
+                        template.actions[action](...args)(localModel(template.sockets, paths))
+                    acc[action] = (...args: any[]) => 
+                        updateStream(syncModel(
+                            postActionModel(...args),
+                            template.sockets, 
+                            paths)
+                        )
                     return acc
                 },
                 {} as any
@@ -121,11 +144,113 @@ export const createApp = (
 
         if (actions['@init']) actions['@init']()
 
-        return () => template.render({model: model(), actions, outlet: () => []})
+        return () => template.render({model: model(), actions, outlet: outlet})
     }
 
-    const start = (rootElement: HTMLElement, component: Component) => {
-        modelStream.map(model => renderer(rootElement, component))
+    const flattenRoutes = (
+        initialRoutes: FlatRouteMap,
+        root: string,
+        actions: RouteAction[], 
+        components: ComponentConfig[], 
+        routes: RouteMap
+    ): FlatRouteMap => 
+    {
+        const prepareAction = (action: RouteAction, config: ComponentConfig): RouteAction =>
+            (params: any) =>
+                syncModel(
+                    action(params)(localModel(config.template.sockets, config.paths)),
+                    config.template.sockets,
+                    config.paths
+                )
+
+        return Object.keys(routes).reduce((acc, path) => {
+            const route = routes[path]
+            if (typeof route !== 'string') {
+                route.action && actions.push(prepareAction(route.action, route.component))
+                components.push(route.component)
+                acc[root + path] = {
+                    name: route.name,
+                    components: components,
+                    actions: actions
+                }
+                if (route.subroutes) flattenRoutes(
+                    acc,
+                    root + path,
+                    actions.slice(0),
+                    components.slice(0),
+                    route.subroutes
+                )
+            }
+            return acc
+        }, initialRoutes)
+    }
+
+    const createRouting =
+        (routes: RouteMap, topConfig: ComponentConfig, render: RouteRendererFn) => {
+        const history = createHistory()
+
+        const flatRoutes = flattenRoutes({}, '', [], [], routes)
+
+        let lastRoute: Route
+        let lastValues: string
+
+        console.log(flatRoutes)
+
+        const chainComponents =
+            (currentConfig: ComponentConfig, restConfigs: ComponentConfig[]): Component => {
+            const nextComponent = restConfigs.shift()
+            if (nextComponent) {
+                return createComponent(
+                    currentConfig.template,
+                    currentConfig.paths,
+                    chainComponents(nextComponent, restConfigs)
+                )
+            }
+            return createComponent(
+                currentConfig.template,
+                currentConfig.paths,
+                () => undefined
+            )
+        }
+
+        const navigateByPath = (path: string) => {
+            const resolved = urlMapper.map(path, flatRoutes)
+            if (resolved) {
+                const route = resolved.match
+                if (!route.cache) {
+                    route.cache = chainComponents(topConfig, route.components.slice(0))
+                }
+                if (route !== lastRoute || lastValues !== JSON.stringify(resolved.values)) {
+                    route.actions.forEach(action => updateStream(action(resolved.values)))
+                }
+                lastRoute = route
+                lastValues = JSON.stringify(resolved.values)
+                render(route.cache)
+            }
+        }
+
+        history.listen((location, action) => {
+            const path = location.hash.substring(1)
+            navigateByPath(path)
+        })
+
+        return () => navigateByPath(history.location.hash.substring(1))
+    }
+
+    const start = (rootElement: HTMLElement, topConfig: ComponentConfig, routes?: RouteMap) => {
+        if (routes) {
+            const wrappedRenderer = createRouting(
+                routes,
+                topConfig,
+                (component: Component) => renderer(rootElement, component)
+            )
+            modelStream.map(model => wrappedRenderer())
+        } else {
+            const topComponent =
+                createComponent(topConfig.template, topConfig.paths, () => undefined)
+            modelStream.map(model => renderer(rootElement, topComponent))
+        }
+
     }
 
     return {
