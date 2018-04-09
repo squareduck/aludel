2e2005187a8192eca20d36331d2a9676661b19f1
Daniil Velichko <danvelduck@gmail.com>
2018-04-09 18:46:49 +0800

    Add CRUD operations, validation and defaults to Models

--- src/model.ts
+++ src/model.ts
@@ -1,13 +1,175 @@
+import { LocalModel } from './context'
+import { Partial } from './component'
+import uuid from 'uuid/v4'
+
+export type ValidationFn = (value) => boolean
+export type ValidationField = string | ValidationFn
+
+export type ModelConfig = {
+    fields?: { [key: string]: ValidationField }
+    defaults?: { [key: string]: any }
+}
+
+export type LocalModel = {
+    path: string[]
+    insert: (state: PartialModelState, instance: LocalModel) => string
+    get: (state: PartialModelState, id: string) => LocalModel
+    filter: (
+        state: PartialModelState,
+        filter: (instance: LocalModel) => boolean,
+    ) => LocalModel[]
+    update: (
+        state: PartialModelState,
+        id: string,
+        change: (instance: LocalModel) => LocalModel,
+    ) => LocalModel
+    remove: (state: PartialModelState, id: string) => boolean
+}
+
+export type ModelState = {
+    collection?: LocalModel
+}
+
+export type PartialModelState = Partial<ModelState>
+
 function modelPath(name: string) {
     return ['$model', name]
 }
 
-export type DataModel = {
-    path: string[]
+function insert(
+    state: PartialModelState,
+    config: ModelConfig,
+    instance: LocalModel,
+): string {
+    instance = applyDefaults(config.defaults, instance)
+    instance = validate(config.fields, instance)
+
+    const id = uuid()
+    if (!state.collection) state.collection = {}
+
+    state.collection[id] = Object.assign(instance, { id })
+    return id
 }
 
-export function createModel(name: string): DataModel {
+function get(state: PartialModelState, id: string): LocalModel {
+    if (!state.collection) return
+
+    return state.collection[id]
+}
+
+function filter(
+    state: PartialModelState,
+    filter: (instance: LocalModel) => boolean,
+): LocalModel[] {
+    if (!state.collection) return []
+
+    return Object.values(state.collection).filter(filter)
+}
+
+function update(
+    state: PartialModelState,
+    config: ModelConfig,
+    id: string,
+    change: (instance: LocalModel) => LocalModel,
+) {
+    if (!state.collection) return
+
+    const user = get(state, id)
+    if (!user) return
+
+    const updatedUser = { ...validate(config.fields, change(user)), id }
+
+    state.collection[id] = updatedUser
+
+    return updatedUser
+}
+
+function remove(state: PartialModelState, id: string): boolean {
+    if (!state.collection) return
+
+    if (!state.collection[id]) return false
+
+    if (state.collection[id]) {
+        delete state.collection[id]
+        return true
+    }
+}
+
+function validate(
+    fields: { [key: string]: ValidationField } = {},
+    instance: LocalModel,
+): LocalModel {
+    const fieldNames = Object.keys(fields)
+
+    // When Model does not have fields we skip validation
+    if (fieldNames.length === 0) return instance
+
+    // When not all fields are present in instance we throw
+    const missingFields = fieldNames.reduce((acc, name) => {
+        return instance.hasOwnProperty(name) ? acc : [...acc, name]
+    }, [])
+    if (missingFields.length > 0)
+        throw new Error(
+            `Model instance is missing fields: ${missingFields.join(', ')}`,
+        )
+
+    // When not all fields are valid we throw
+    const invalidFields = fieldNames.reduce((acc, name) => {
+        const validator = fields[name]
+        let valid = false
+
+        if (typeof validator === 'string') {
+            valid = typeof instance[name] === validator
+        } else {
+            valid = validator(instance[name])
+        }
+
+        if (!valid) return [...acc, name]
+
+        return acc
+    }, [])
+
+    if (invalidFields.length > 0)
+        throw new Error(
+            `Model instance has invalid fields: ${invalidFields.join(', ')}`,
+        )
+
+    return fieldNames.reduce((acc, name) => {
+        acc[name] = instance[name]
+        return acc
+    }, {})
+}
+
+function applyDefaults(
+    defaults: { [key: string]: any } = {},
+    instance: LocalModel,
+) {
+    const computedDefaults = Object.keys(defaults).reduce((acc, name) => {
+        if (typeof defaults[name] === 'function') {
+            acc[name] = defaults[name]()
+        } else {
+            acc[name] = defaults[name]
+        }
+
+        return acc
+    }, {})
+
+    return {
+        ...computedDefaults,
+        ...instance,
+    }
+}
+
+export function createModel(
+    name: string,
+    config: ModelConfig = {},
+): LocalModel {
     return {
         path: modelPath(name),
+        insert: (state, instance) => insert(state, config, instance),
+        get: (state, id) => get(state, id),
+        filter: (state, fn) => filter(state, fn),
+        update: (state, id, change) => update(state, config, id, change),
+        remove: (state, id) => remove(state, id),
     }
 }
--- test/model_test.ts
+++ test/model_test.ts
@@ -1,7 +1,221 @@
 import test from 'ava'
 import { createModel } from '../src/model'
 
-test('createModel() exposes path', t => {
+test('createModel().path returns model path for Global State', t => {
     const model = createModel('User')
     t.deepEqual(model.path, ['$model', 'User'])
+})
+
+test('createModel().insert() puts value in model collection', t => {
+    const state = {}
+    const model = createModel('User')
+    const id = model.insert(state, { name: 'John', age: 21 })
+
+    t.deepEqual(state['collection'][id], { id, name: 'John', age: 21 })
+})
+
+test('createModel().insert() respects default values', t => {
+    const state = {}
+    const model = createModel('User', {
+        defaults: {
+            name: 'Default',
+            age: () => 21,
+        },
+    })
+
+    const id = model.insert(state, { email: 'test@example.com' })
+
+    t.deepEqual(model.get(state, id), {
+        id,
+        name: 'Default',
+        age: 21,
+        email: 'test@example.com',
+    })
+})
+
+test('createModel().insert() respects validation', t => {
+    const state = {}
+    const model = createModel('User', {
+        fields: {
+            name: 'string',
+            age: n => n > 20,
+            email: 'string',
+        },
+    })
+
+    // Checks that all fields are present
+    const amountErr = t.throws(() => {
+        model.insert(state, {
+            name: 'John',
+        })
+    })
+    t.is(amountErr.message, 'Model instance is missing fields: age, email')
+
+    // Runs validators for all fields
+    const ageErr = t.throws(() => {
+        model.insert(state, {
+            name: 'John',
+            age: 15,
+            email: null,
+        })
+    })
+    t.is(ageErr.message, 'Model instance has invalid fields: age, email')
+
+    // Ignores unspecified fields in instance
+    const id = model.insert(state, {
+        name: 'John',
+        age: 21,
+        email: 'test@example.com',
+        hobby: 'music',
+    })
+    t.deepEqual(model.get(state, id), {
+        id,
+        name: 'John',
+        age: 21,
+        email: 'test@example.com',
+    })
+})
+
+test('createModel().insert() applies defaults before validation', t => {
+    const state = {}
+    const model = createModel('User', {
+        fields: {
+            name: 'string',
+            age: n => n > 20,
+        },
+        defaults: {
+            name: 'Default',
+            age: 21,
+        },
+    })
+
+    const ageErr = t.throws(() => {
+        model.insert(state, {
+            age: 18,
+        })
+    })
+    t.is(ageErr.message, 'Model instance has invalid fields: age')
+
+    const id = model.insert(state, {})
+
+    t.deepEqual(model.get(state, id), {
+        id,
+        name: 'Default',
+        age: 21,
+    })
+})
+
+test('createModel().get() searches instance by id', t => {
+    const state = {}
+    const model = createModel('User')
+    const id = model.insert(state, { name: 'John', age: 21 })
+
+    const user = model.get(state, id)
+
+    t.deepEqual(user, state['collection'][id])
+})
+
+test('createModel().filter() filters instances by function', t => {
+    const state = {}
+    const model = createModel('User')
+
+    const johnId = model.insert(state, { name: 'John', age: 21 })
+    const ashId = model.insert(state, { name: 'Ash', age: 18 })
+    const bobId = model.insert(state, { name: 'Bob', age: 32 })
+
+    t.deepEqual(model.filter(state, i => i.age > 20), [
+        model.get(state, johnId),
+        model.get(state, bobId),
+    ])
+
+    t.deepEqual(model.filter(state, i => i.age < 20), [model.get(state, ashId)])
+
+    t.deepEqual(model.filter(state, i => i.age === 20), [])
+})
+
+test('createModel().update() mutates instance by id', t => {
+    const state = {}
+    const model = createModel('User')
+
+    const id = model.insert(state, { name: 'John', age: 21 })
+
+    const updatedUser = model.update(state, id, user => {
+        user.name = 'Johny'
+        return user
+    })
+
+    t.is(updatedUser.id, id)
+    t.is(updatedUser.name, 'Johny')
+    t.is(updatedUser.age, 21)
+
+    const john = model.get(state, id)
+
+    t.is(john.name, 'Johny')
+    t.is(john.age, 21)
+})
+
+test('createModel().update() respects validation', t => {
+    const state = {}
+    const model = createModel('User', {
+        fields: {
+            name: 'string',
+            age: n => n > 20,
+            email: 'string',
+        },
+    })
+
+    const id = model.insert(state, {
+        name: 'John',
+        age: 21,
+        email: 'test@example.com',
+    })
+
+    // Checks that all fields are present
+    const amountErr = t.throws(() => {
+        model.update(state, id, user => {
+            return { name: user.name }
+        })
+    })
+    t.is(amountErr.message, 'Model instance is missing fields: age, email')
+
+    // Runs validators on all fields
+    const ageErr = t.throws(() => {
+        model.update(state, id, user => {
+            return { ...user, age: 18 }
+        })
+    })
+    t.is(ageErr.message, 'Model instance has invalid fields: age')
+    t.is(model.get(state, id).age, 21)
+
+    // Ignores unspecified fields in instance
+    const newUser = model.update(state, id, user => ({
+        ...user,
+        age: 27,
+        hobby: 'music',
+    }))
+
+    t.deepEqual(newUser, {
+        id,
+        name: 'John',
+        age: 27,
+        email: 'test@example.com',
+    })
+
+    t.deepEqual(model.get(state, id), newUser)
+})
+
+test('createModel().remove() deletes instance from collection by id', t => {
+    const state = {}
+    const model = createModel('User')
+
+    t.is(model.remove(state, 'nosuchid'), undefined)
+
+    const id = model.insert(state, { name: 'John', age: 21 })
+
+    t.is(model.remove(state, 'nosuchid'), false)
+
+    const result = model.remove(state, id)
+
+    t.is(result, true)
+    t.is(model.get(state, id), undefined)
 })
