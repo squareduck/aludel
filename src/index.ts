d9056a03aeaa0f023aaf058236defbc5f38535e5
Daniil Velichko <danvelduck@gmail.com>
2018-04-12 16:16:42 +0800

    Add createModel to index exports

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,4 @@ export {
 export { createContext } from './context'
 export { createApp, createRoutedApp } from './app'
 export { createRouter, RouteMap } from './router'
+export { createModel } from './model'
